2eae5f63ad945e988bf3b59b868212feb5494542
Ian <ian.njoroge2@student.moringaschool.com>
2025-03-08 11:08:49 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+module.exports = { writeCards };
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+module.exports = { countDown };
